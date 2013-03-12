42658f65fd395c4ba098de0ab9fa3fc656a8ecf3
James Healy <james@yob.id.auto>
2013-03-12 22:09:03 +1100

    foo

--- lib/selenium/webdriver/firefox/socket_lock.rb
+++ lib/selenium/webdriver/firefox/socket_lock.rb
@@ -47,7 +47,7 @@ module Selenium
 
           true
         rescue SocketError, Errno::EADDRINUSE, Errno::EBADF => ex
-          $stderr.puts "#{ENV["SPEC_PARALLEL"]}: #{self}: #{ex.class}: #{ex.message}"
+          $stderr.puts "#{self}: #{ex.class}: #{ex.message}"
           false
         end
 
